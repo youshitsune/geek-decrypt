c099cc5d1d6f54e1e4e6378fe6121e24389862b1
youshitsune <luka@youshitsune.me>
2023-04-20 19:13:26 +0200

    Implement 33rd part

--- main.py
+++ main.py
@@ -290,7 +290,16 @@ flags = {
         "e": "Finished High School",
         "e-": "Haven't finished High School",
         "e--": "Haven't even entered High School",
-        "e*": "I learned everything there is to know about life from the \"Hitchhiker's Trilogy\"."
+        "e*": "I learned everything there is to know about life from the \"Hitchhiker's Trilogy\".",
+        "h++": "Living in a cave with 47 computers and an Internet feed, located near a Dominoes pizza. See !d.",
+        "h+": "Living alone, get out once a week to buy food, no more than once a month to do laundry. All surfaces covered.",
+        "h": "Friends come over to visit every once in a while to talk about Geek things. There is a place for them to sit.",
+        "h-": "Living with one or more registered Geeks.",
+        "h--": "Living with one or more people who know nothing about being a Geek and refuse to watch Babylon 5.",
+        "h---": "Married, (persons living romantically with someone might as well label themselves h---, you're as good as there already.)",
+        "h----": "Married with children - Al Bundy can sympathize",
+        "h!": "I am stuck living with my parents!",
+        "h*": "I'm not sure where I live anymore. This lab/workplace seems like home to me."
         }
 
 dec = []
